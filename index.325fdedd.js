922ecff7fdb4dd967b567407dae3a502dcf82b59
iryna lysko <goiralysko@gmail.com>
2024-08-14 15:10:10 +0300

    make build

---
+++ index.325fdedd.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".gallery__list"),t=document.querySelector("#largeImg");e.addEventListener("click",function(e){e.preventDefault();var r=e.target.closest(".list-item").querySelector(".list-item__link");t.src=r.href});
+//# sourceMappingURL=index.325fdedd.js.map
